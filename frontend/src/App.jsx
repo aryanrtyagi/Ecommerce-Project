c0884717c6da453f78c3ceb6d70583d1e713e20f
aryanrtyagi <aryan210609@gmail.com>
2026-05-18 01:15:56 +0530

    About

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,21 +15,23 @@ import Footer from './components/Footer';
 function App() {
   return (
     <Router>
-      <Toaster position='top-center'/>
+      <Toaster position='top-center' />
       <Navbar />
-      <Routes>
-        <Route path="/" element={<ProductList />} />
-        <Route path="/product/:id" element={<ProductDetails />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route element={<PrivateRouter />}>
-          <Route path='/checkout' element={<CheckoutPage />} />
-        </Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/category/:id" element={<CategoryProducts />} />
-        <Route path="/products" element={<ProductsPage />} />
-      </Routes>
-      <Footer />
+      <div className='pt-20'>
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route element={<PrivateRouter />}>
+            <Route path='/checkout' element={<CheckoutPage />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/category/:id" element={<CategoryProducts />} />
+          <Route path="/products" element={<ProductsPage />} />
+        </Routes>
+        <Footer />
+      </div>
     </Router>
   )
 }
