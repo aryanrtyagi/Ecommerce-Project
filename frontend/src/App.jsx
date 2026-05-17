ec2e3fe03ac9a0bab37afe8a29300172f4905553
aryanrtyagi <aryan210609@gmail.com>
2026-05-18 01:02:59 +0530

    About

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,45 +1,22 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import {Toaster} from "react-hot-toast";
-import ProductList from "./pages/ProductList";
-import ProductDetails from "./pages/ProductDetails";
-import Navbar from './components/Navbar';
-import CartPage from "./pages/CartPage";
-import CheckoutPage from './pages/CheckoutPage';
-import PrivateRouter from './components/PrivateRouter';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import CategoryProducts from "./pages/CategoryProducts";
-import ProductsPage from './pages/ProductPage';
-import Footer from './components/Footer';
-
 function App() {
   return (
-    <>
+    <Router>
       <Toaster position='top-center'/>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<ProductList />} />
-          <Route path="/product/:id" element={<ProductDetails />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route elememt={<PrivateRouter />}>
-            <Route path='/CheckOut' element={<CheckoutPage />} />
-          </Route>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route
-            path="/category/:id"
-            element={<CategoryProducts />}
-          />
-          <Route
-            path="/products"
-            element={<ProductsPage />}
-          />
-        </Routes>
-        <Footer/>
-      </Router>
-      
-    </>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route element={<PrivateRouter />}>
+          <Route path='/checkout' element={<CheckoutPage />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/category/:id" element={<CategoryProducts />} />
+        <Route path="/products" element={<ProductsPage />} />
+      </Routes>
+      <Footer />
+    </Router>
   )
 }
 
