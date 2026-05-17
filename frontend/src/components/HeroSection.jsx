512b69ccacae7a8ebfcca4a35b3b859e842c5f9d
aryanrtyagi <aryan210609@gmail.com>
2026-05-18 00:42:09 +0530

    final push

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -12,7 +12,7 @@ function HeroSection() {
         fetch(`${BASE}/api/products/`)
             .then((res) => res.json())
             .then((data) => {
-                const FEATURED_IDS = [1,2,3,9]
+                const FEATURED_IDS = [10,11,12]
                 setProducts(data.filter((p) => FEATURED_IDS.includes(p.id)));
             }) // first n products
             .catch((err) => console.error("Failed to fetch products:", err));
